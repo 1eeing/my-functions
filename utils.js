c4bc6d345a7d28a2e4e1518254d60b31f90de8be
leeing <372711472@qq.com>
2019-03-25 23:58:22 +0800

    手写去抖和节流

--- utils.js
+++ utils.js
@@ -12,3 +12,24 @@ export const compose = (...args) => {
         return f1(res);
     }
 };
+
+export const debounce = (fn, delay) => {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        setTimeout(() => {
+            fn.apply(this, args);
+        }, delay)
+    }
+};
+
+export const throttle = (fn, delay) => {
+    let last = 0;
+    return function(...args) {
+        const cur = Date.now();
+        if(cur - last > delay){
+            fn.apply(this, args);
+            last = cur;
+        }
+    }
+};
