22fa0e57417e0ac1f78863c9f19cab5a951e5d5e
leeing <lining@growingio.com>
2020-05-27 11:22:45 +0800

    新增数组查重，二分查找的思路

---
+++ src/structure/findRepeat.ts
@@ -0,0 +1,21 @@
+/**
+ * 查找数组中某一项出现的次数
+ */
+
+function find<T>(arr: T[], k: T){
+  let count = 0;
+  const handler = (_arr: T[], _k: T) => {
+      if(!_arr || !_arr.length || _k == void 0) return;
+      const midIndex = Math.floor(_arr.length - 1);
+      const mid = _arr[midIndex];
+      const left = _arr.slice(0, midIndex);
+      const right = _arr.slice(midIndex + 1, _arr.length);
+      if(mid === _k){
+          count ++;
+      }
+      if(left.length) handler(left, k);
+      if(right.length) handler(right, k);
+  }
+  handler(arr, k);
+  return count;
+}
