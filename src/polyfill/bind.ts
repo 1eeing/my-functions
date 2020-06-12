854ccc71520100b31636c9d5eda0daea16f2bff2
leeing <lining@growingio.com>
2020-06-12 18:28:05 +0800

    Update

--- src/polyfill/bind.ts
+++ src/polyfill/bind.ts
@@ -1,4 +1,7 @@
-Function.prototype.bind = function(target, ...args) {
+Function.prototype.bind = function (target, ...args) {
+  if (typeof target !== 'function') {
+    throw new TypeError('targte must to be a function');
+  }
   const fn = this;
   return function(...args1) {
       return fn.apply(target, args.concat(args1));
