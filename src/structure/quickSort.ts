ecd1b4b44b22ab603db20dd1a52c99d7390dcc6a
leeing <lining@growingio.com>
2020-07-31 15:26:45 +0800

    Update

--- src/structure/quickSort.ts
+++ src/structure/quickSort.ts
@@ -3,12 +3,17 @@
  */
 
 function quickSort(arr: number[]) {
+  // 递归的结束条件是元素小于等于1个
   if (arr.length <= 1) {
       return arr;
   }
+  // 先把最后一个元素找出来
   const mid = arr[arr.length - 1];
-  const left = arr.filter((item, index) => item < mid && index < arr.length - 1 );
+  // 比该元素小的放左边
+  const left = arr.filter((item, index) => item < mid && index < arr.length - 1);
+  // 比该元素大的放右边
   const right = arr.filter(item => item > mid);
+  // 递归该过程
   return [...quickSort(left), mid, ...quickSort(right)]
 }
 
